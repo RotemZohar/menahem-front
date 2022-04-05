c5fa0d19167ff456fda60f15e8423d8ed8369c57
rotem <rzrotem@gmail.com>
2022-04-05 20:13:07 +0300

    Add use fetch

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Provider as UseFetchProvider } from "use-http";
 import App from "./App";
 import { persistor, store } from "./redux/store";
 
+const BACK_API = process.env.REACT_APP_BACK_API;
+
+console.log(BACK_API);
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ReduxProvider store={store}>
+      <UseFetchProvider url={BACK_API}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </UseFetchProvider>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
