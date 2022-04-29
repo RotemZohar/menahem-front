1684cba09f26ee9476bdfe913f1ac361bf8a93af
rotem <rzrotem@gmail.com>
2022-04-29 17:20:47 +0300

    Add private route component

---
+++ src/components/private-route/PrivateRoute.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RouteProps, Outlet, Navigate } from "react-router-dom";
+import { routes } from "../../routes";
+
+const PrivateRoute: React.FC<RouteProps> = () => {
+  // TODO: check if logged in, if not then redirect. need the JWT PR for that
+  if (true) {
+    return <Navigate to={routes.signin} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
