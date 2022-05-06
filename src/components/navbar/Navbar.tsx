dec08cdfc91dbbdd4c5d4ea3192e832d202fbc3c
rotem <rzrotem@gmail.com>
2022-05-06 19:35:00 +0300

    Change navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,50 @@
-import { Favorite } from "@mui/icons-material";
-import { BottomNavigation, BottomNavigationAction, Icon } from "@mui/material";
-import React from "react";
+import { CalendarViewMonth, Favorite } from "@mui/icons-material";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Button,
+  Grid,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import calendarPng from "../../assets/calendar.png";
+import { setShowNavbar } from "../../redux/slices/navbarSlice";
 
 function Navbar() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <BottomNavigation>
-      <BottomNavigationAction
-        value="my-pets"
-        label="pets"
-        icon={
-          <Icon>
-            <img alt="" src={calendarPng} />
-          </Icon>
-        }
-      />
-      <BottomNavigationAction value="my-tasks" />
-      <BottomNavigationAction value="my-pets" />
-      <BottomNavigationAction value="my-pets" />
-    </BottomNavigation>
+    <Grid container justifyContent="space-around">
+      <Grid item>
+        <Button onClick={handleClick}>
+          <img src={calendarPng} height="50" width="50" alt="calendar" />
+        </Button>
+        <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </Grid>
+      <Grid item>
+        <Button>
+          <img src={calendarPng} height="50" width="50" alt="calendar" />
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button>
+          <img src={calendarPng} height="50" width="50" alt="calendar" />
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
