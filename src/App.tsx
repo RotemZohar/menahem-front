2d186678a85ad4d7d3fc1a9f6b6bf09c260d2058
rotem <rzrotem@gmail.com>
2022-05-06 16:17:46 +0300

    Add center

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,18 @@ import { routes } from "./routes";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 
 const App = () => (
-  <Routes>
-    <Route path="" element={<LandingPage />} />
-    <Route path={routes.signup} element={<SingupPage />} />
-    {/* Everything that's inside private route is accessible only after logging in */}
-    <Route element={<PrivateRoute />}>
-      <Route path="posts" element={<PostsPage />} />
-      <Route path="editDetails" element={<EditDetailsPage />} />
-      <Route path="admin" element={<AdminPage />} />
-    </Route>
-  </Routes>
+  <div className="App">
+    <Routes>
+      <Route path="" element={<LandingPage />} />
+      <Route path={routes.signup} element={<SingupPage />} />
+      {/* Everything that's inside private route is accessible only after logging in */}
+      <Route element={<PrivateRoute />}>
+        <Route path="posts" element={<PostsPage />} />
+        <Route path="editDetails" element={<EditDetailsPage />} />
+        <Route path="admin" element={<AdminPage />} />
+      </Route>
+    </Routes>
+  </div>
 );
 
 export default App;
