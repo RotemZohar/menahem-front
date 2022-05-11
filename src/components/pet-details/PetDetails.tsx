d67f8bcb3680522843eaebd70c43f545006d7677
Eran <eran@nizry.com>
2022-05-11 14:39:27 +0300

    Changed query to use useFetch

--- src/components/pet-details/PetDetails.tsx
+++ src/components/pet-details/PetDetails.tsx
@@ -8,143 +8,146 @@ import PetCarers from "./PetCarers";
 import PetMedical from "./PetMedical";
 
 function TabPanel(props: { index: number; value: number; children: any }) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 function getAge(birthdate: Date) {
-  const today = new Date();
-  birthdate = new Date(birthdate);
-  let age = today.getFullYear() - birthdate.getFullYear();
-  const m = today.getMonth() - birthdate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
-    age -= 1;
-  }
-  return age;
+    const today = new Date();
+    birthdate = new Date(birthdate);
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const m = today.getMonth() - birthdate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+        age -= 1;
+    }
+    return age;
 }
 
 const PetDetails = () => {
-  const { get } = useFetch("/auth");
-  const [value, setValue] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-  const [details, setDetails] = useState<petDetails>({
-    _id: "",
-    name: "",
-    medical: [
-      {
+    const { get } = useFetch("/auth");
+    const [value, setValue] = useState(0);
+    const [loaded, setLoaded] = useState(false);
+    const [details, setDetails] = useState<petDetails>({
         _id: "",
-        treatment: "",
-        date: new Date(),
-      },
-    ],
-    members: [
-      {
-        _id: "",
-        isAdmin: false,
-      },
-    ],
-    tasks: [
-      {
-        _id: "",
-        title: "",
-        description: "",
-        dateFrom: new Date(),
-        dateTo: new Date(),
-        isCompleted: false,
-      },
-    ],
-    birthdate: new Date(),
-    breed: "",
-    height: "",
-    weight: "",
-    imgUrl: "",
-  });
+        name: "",
+        medical: [
+            {
+                _id: "",
+                treatment: "",
+                date: new Date(),
+            },
+        ],
+        members: [
+            {
+                _id: "",
+                isAdmin: false,
+            },
+        ],
+        tasks: [
+            {
+                _id: "",
+                title: "",
+                description: "",
+                dateFrom: new Date(),
+                dateTo: new Date(),
+                isCompleted: false,
+            },
+        ],
+        birthdate: new Date(),
+        breed: "",
+        height: "",
+        weight: "",
+        imgUrl: "",
+    });
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
-  useEffect(() => {
-    get("/pet?_id=627403102576b2374e417745")
-      .then((res) => res.json())
-      .then((data) => {
-        setDetails(data);
-        setLoaded(true);
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    useEffect(() => {
+        // Change _id to parameter when able to recieve
+        get("/pet?_id=627403102576b2374e417745")
+            .then((res) => {
+                setDetails(res);
+                setLoaded(true);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-  if (!loaded)
+    if (!loaded)
+        return (
+            <div>
+                <h1> Loading... </h1>
+            </div>
+        );
+
     return (
-      <div>
-        <h1> Loading... </h1>
-      </div>
+        <Container>
+            <Box padding={3} sx={{ border: 2, borderBottom: 1 }} mt={4} mx={20}>
+                <Box display="flex">
+                    <Avatar
+                        src={details.imgUrl}
+                        sx={{ width: 160, height: 160, border: 2 }}
+                    />
+                    <Box mx={3} mt={3}>
+                        <Typography variant="h2" align="left">
+                            {details.name}
+                        </Typography>
+                        <Typography variant="h5" align="left">
+                            {details.breed}, {getAge(details.birthdate)}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box display="flex" mt={1}>
+                    <Typography variant="h6" align="left" ml={3}>
+                        height: {details.height}cm
+                    </Typography>
+                    <Typography variant="h6" align="left" ml={5}>
+                        weight: {details.weight}kg
+                    </Typography>
+                </Box>
+            </Box>
+            <Box sx={{ border: 2, borderTop: 0 }} mx={20}>
+                <Box sx={{ borderBottom: 1 }}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        variant="fullWidth"
+                    >
+                        <Tab label="Medical" sx={{ borderRight: 1 }} />
+                        <Tab label="Carers" sx={{ borderRight: 1 }} />
+                        <Tab label="Tasks" />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <PetMedical medical={details.medical} />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <PetCarers carers={details.members} />
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <PetTasks tasks={details.tasks} />
+                </TabPanel>
+            </Box>
+        </Container>
     );
-
-  return (
-    <Container>
-      <Box padding={3} sx={{ border: 2, borderBottom: 1 }} mt={4} mx={20}>
-        <Box display="flex">
-          <Avatar
-            src={details.imgUrl}
-            sx={{ width: 160, height: 160, border: 2 }}
-          />
-          <Box mx={3} mt={3}>
-            <Typography variant="h2" align="left">
-              {details.name}
-            </Typography>
-            <Typography variant="h5" align="left">
-              {details.breed}, {getAge(details.birthdate)}
-            </Typography>
-          </Box>
-        </Box>
-        <Box display="flex" mt={1}>
-          <Typography variant="h6" align="left" ml={3}>
-            height: {details.height}cm
-          </Typography>
-          <Typography variant="h6" align="left" ml={5}>
-            weight: {details.weight}kg
-          </Typography>
-        </Box>
-      </Box>
-      <Box sx={{ border: 2, borderTop: 0 }} mx={20}>
-        <Box sx={{ borderBottom: 1 }}>
-          <Tabs value={value} onChange={handleChange} variant="fullWidth">
-            <Tab label="Medical" sx={{ borderRight: 1 }} />
-            <Tab label="Carers" sx={{ borderRight: 1 }} />
-            <Tab label="Tasks" />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          <PetMedical medical={details.medical} />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <PetCarers carers={details.members} />
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <PetTasks tasks={details.tasks} />
-        </TabPanel>
-      </Box>
-    </Container>
-  );
 };
 
 export default PetDetails;
