6cdefd99d31c3d810505131006a878e4bb6f0082
erannizry <erannizry99@gmail.com>
2022-05-18 11:16:36 +0300

    Added groups tab: WIP

---
+++ src/components/pet-details/PetGroups.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const PetGroups = (props: { groups: any[] }) => {
+  const { groups } = props;
+  return <div>Not implemented yet</div>;
+};
+
+export default PetGroups;
