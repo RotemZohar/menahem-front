a3006f985948c5834fd9ab74d7742d94f2407a3c
rotem <rzrotem@gmail.com>
2022-05-20 19:03:50 +0300

    Add name and description

--- src/components/create-group/CreateGroupPage.tsx
+++ src/components/create-group/CreateGroupPage.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import React, { FormEvent, useState } from "react";
 
 function CreateGroupPage() {
-  return <div>CreateGroup</div>;
+  const [groupName, setGroupName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // TODO: save group
+  };
+
+  return (
+    <>
+      <Typography variant="h3" marginBottom={2}>
+        Create a new group
+      </Typography>
+      <form
+        style={{
+          display: "inline-flex",
+          flexDirection: "column",
+          gap: 10,
+        }}
+        onSubmit={onSubmit}
+      >
+        <TextField
+          value={groupName}
+          onChange={(e) => setGroupName(e.target.value)}
+          label="Group name"
+        />
+        <TextField
+          value={description}
+          multiline
+          onChange={(e) => setDescription(e.target.value)}
+          label="Description"
+        />
+        <Button type="submit">Create</Button>
+      </form>
+    </>
+  );
 }
 
 export default CreateGroupPage;
