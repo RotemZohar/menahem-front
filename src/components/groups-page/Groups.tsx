09a20131fe9cea2b37c858274b65e5006e39acb0
Rom <romkadria@gmail.com>
2022-05-22 23:37:48 +0300

    groups screen

--- src/components/groups-page/Groups.tsx
+++ src/components/groups-page/Groups.tsx
@@ -1,38 +1,67 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import useFetch from "use-http";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import { Group } from "../../types/group";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
+const GroupsPage = () => {
+  const [groups, setGroups] = useState<Group[]>([]);
+  const { get } = useFetch("/auth");
 
-const GroupsPage = () => (
-  <Card sx={{ minWidth: 275 }}>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">well meaning and kindly.</Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-);
+  useEffect(() => {
+    get("/user/groups?id=627b5d24258aaa8a7c084780")
+      .then((data) => {
+        setGroups(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  const navToGroup = (group: Group) => {
+    console.log("Navigating to group");
+  };
+
+  const addGroup = () => {
+    console.log("add group");
+  };
+
+  const groupsCards = useMemo(
+    () =>
+      groups.map((group) => (
+        <CardActionArea onClick={() => navToGroup(group)}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {group.name}
+              </Typography>
+            </CardContent>
+          </Card>
+        </CardActionArea>
+      )),
+    [groups]
+  );
+
+  return (
+    <Box>
+      <Typography variant="h2" gutterBottom>
+        My groups
+      </Typography>
+      {groupsCards}
+      <Fab onClick={addGroup} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+    </Box>
+  );
+};
 export default GroupsPage;
