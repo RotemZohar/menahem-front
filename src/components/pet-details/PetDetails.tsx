0b8b2ce15cd7af2fe317f0accc4dbd9feeccf876
Eran <eran@nizry.com>
2022-05-23 21:46:55 +0300

    age is now const

--- src/components/pet-details/PetDetails.tsx
+++ src/components/pet-details/PetDetails.tsx
@@ -11,10 +11,10 @@ import TabPanel from "../tab-panel/TabPanel";
 const getAge = (birthdate: Date) => {
   const today = new Date();
   birthdate = new Date(birthdate);
-  let age = today.getFullYear() - birthdate.getFullYear();
+  const age = today.getFullYear() - birthdate.getFullYear();
   const m = today.getMonth() - birthdate.getMonth();
   if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
-    age -= 1;
+    return age - 1;
   }
   return age;
 };
