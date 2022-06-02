eeb47f8caad99cfcb87cdb4f06c5cf2d1d6e6352
Rom <romkadria@gmail.com>
2022-06-02 18:35:54 +0300

    change style

--- src/components/pet-medical-page-guests/petMedicalPageGuests.tsx
+++ src/components/pet-medical-page-guests/petMedicalPageGuests.tsx
@@ -11,7 +11,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import moment from "moment";
-import { Tab, Tabs } from "@mui/material";
+import { Grid, Tab, Tabs } from "@mui/material";
 import { useState } from "react";
 import { Treatment } from "../../types/pet";
 import TabPanel from "../tab-panel/TabPanel";
@@ -41,70 +41,69 @@ const PetMedicalPageGuests = () => {
       {loading && "Loading..."}
       {petMedical && petMedical.medical && (
         <Box>
-          <Tabs value={value} onChange={handleChange} variant="fullWidth">
+          <Tabs value={value} onChange={handleChange}>
             <Tab label="Medical" />
             <Tab label="Carers" />
-            <TabPanel value={value} index={0}>
-              <Typography variant="h2" gutterBottom>
-                Pet medical history
-              </Typography>
-
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell align="center">Treatment</TableCell>
-                      <TableCell align="center">Date</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {petMedical.medical.map((row: Treatment) => (
-                      <TableRow
-                        key={row._id}
-                        sx={{
-                          "&:last-child td, &:last-child th": { border: 0 },
-                        }}
-                      >
-                        <TableCell align="center">{row.treatment}</TableCell>
-                        <TableCell align="center">
-                          {moment(row.date).format("DD-MM-YYYY")}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <Typography variant="h2" gutterBottom>
-                Pet carers
-              </Typography>
-
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell align="center">Name</TableCell>
-                      <TableCell align="center">Email</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {petMedical.members.map((row: User) => (
-                      <TableRow
-                        key={row._id}
-                        sx={{
-                          "&:last-child td, &:last-child th": { border: 0 },
-                        }}
-                      >
-                        <TableCell align="center">{row.name}</TableCell>
-                        <TableCell align="center">{row.email}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </TabPanel>
           </Tabs>
+          <TabPanel value={value} index={0}>
+            <Typography variant="h2" gutterBottom>
+              Pet medical history
+            </Typography>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Treatment</TableCell>
+                    <TableCell align="center">Date</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {petMedical.medical.map((row: Treatment) => (
+                    <TableRow
+                      key={row._id}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell align="center">{row.treatment}</TableCell>
+                      <TableCell align="center">
+                        {moment(row.date).format("DD-MM-YYYY")}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Typography variant="h2" gutterBottom>
+              Pet carers
+            </Typography>
+
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Name</TableCell>
+                    <TableCell align="center">Email</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {petMedical.members.map((row: User) => (
+                    <TableRow
+                      key={row._id}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell align="center">{row.name}</TableCell>
+                      <TableCell align="center">{row.email}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
           {/* <Typography variant="h2" gutterBottom>
             Pet medical history
           </Typography>
